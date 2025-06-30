c33e3a1218d756bbe7893e069777c8df454d4a64
Dan Berliner <danberliner@protonmail.com>
2025-06-30 12:05:15 -0500

    Improve speed by having writepx implementation store rows and send them in bulk

--- Src/ili9341_driver.c
+++ Src/ili9341_driver.c
@@ -1,7 +1,13 @@
 #include "ili9341.h"
 #include "main.h"
+#include <string.h>
 
 static SPI_HandleTypeDef *_hspi = NULL;
+
+/* This device only has 16k ram and it would take ~178k to buffer a full colored frame */
+/* Rows of 240 18-byte pixels are byte alligned (540 bytes) */
+static uint8_t _vrambuf[ILI9341_MAX_X*2] = { 0 };
+static uint8_t _vrambuf_len = 0;
 
 static void reset_pin(ili9341_reset_e status) {
   if (status == RESET_HIGH_NOTSET) {
@@ -40,13 +46,45 @@ void sendbyte(uint8_t b) {
   HAL_SPI_Transmit(_hspi, &b, 1, 1000);
 }
 
+static void commit(void* _unused) {
+  (void) _unused;
+  HAL_SPI_Transmit(_hspi, _vrambuf, _vrambuf_len, 1000);
+  _vrambuf_len = 0;
+}
+
+/* Writes the 18 least significant bits to the outgoing buffer */
+static void sendPx(uint32_t b) {
+  uint8_t *mem_byte = _vrambuf + _vrambuf_len;
+  uint8_t R=0, G=0, B=0;
+  uint8_t colorBuf[2] = { 0 };
+
+  /* TODO Support 666 color scheme if that's what is enabled */
+  ILI9341_RGB565_DECODE(b, R, G, B)
+  /* Sent first: The entire R buffer (5 bits) */
+  colorBuf[0] = R<<3;
+  /* Sent second: The most significant bits of G (3 bits) */
+  colorBuf[0] |= (G>>3) & 0x7;
+  /* Sent third: The least significant bits of G (3 bits) */
+  colorBuf[1] = (G & 0x7)<<5;
+  /* Sent fourth: The B buffer (5 bits) */
+  colorBuf[1] |= B & 0x1F;
+  memcpy(mem_byte, colorBuf, sizeof(colorBuf));
+
+  if ((_vrambuf_len+=2) == ILI9341_MAX_X) {
+    commit(NULL);
+  }
+}
+
+
 static ili9341_hw_intf_t stm32f072b_ili9341_driver = {
   .reset_pin=reset_pin,
   .dc_pin=dc_pin,
   .cs_pin=cs_pin,
   .delay=delay,
   .sendbyte=sendbyte,
-  .commit=NULL
+  .sendpx=sendPx,
+  .commit=commit,
+  .async=true
 };
 
 const ili9341_hw_intf_t* stm32f072b_ili9341_setup_driver(SPI_HandleTypeDef *hspi) {
